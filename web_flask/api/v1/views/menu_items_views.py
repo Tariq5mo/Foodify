3f806d2cf268c92a2e5c18faa5c694ee190ab87b
Tariq5mo <tariqomer756@gmail.com>
2025-01-01 15:55:07 +0200

    Add CRUD endpoints for menu items and their reviews in the API

--- web_flask/api/v1/views/menu_items_views.py
+++ web_flask/api/v1/views/menu_items_views.py
@@ -16,3 +16,51 @@ def get_menu_items():
 def get_menu_item(id):
     """Get a specific menu_items"""
     return id
+
+
+@app_views.route("/menu_items", methods=["POST"], strict_slashes=False)
+def post_menu_item():
+    """Create a new menu item"""
+    return "POST"
+
+
+@app_views.route("/menu_items/<id>", methods=["PUT"], strict_slashes=False)
+def put_menu_item(id):
+    """Update a menu item"""
+    return id
+
+
+@app_views.route("/menu_items/<id>", methods=["DELETE"], strict_slashes=False)
+def delete_menu_item(id):
+    """Delete a menu item"""
+    return id
+
+
+@app_views.route("/menu_items/<id>/reviews", strict_slashes=False)
+def get_menu_item_reviews(id):
+    """Get all reviews for a specific menu item"""
+    return id
+
+
+@app_views.route("/menu_items/<id>/reviews", methods=["POST"], strict_slashes=False)
+def post_menu_item_reviews(id):
+    """Create a new review for a specific menu item"""
+    return id
+
+
+@app_views.route("/menu_items/<id>/reviews/<review_id>", strict_slashes=False)
+def get_menu_item_review(id, review_id):
+    """Get a specific review for a specific menu item"""
+    return id
+
+
+@app_views.route("/menu_items/<id>/reviews/<review_id>", methods=["PUT"], strict_slashes=False)
+def put_menu_item_review(id, review_id):
+    """Update a specific review for a specific menu item"""
+    return id
+
+
+@app_views.route("/menu_items/<id>/reviews/<review_id>", methods=["DELETE"], strict_slashes=False)
+def delete_menu_item_review(id, review_id):
+    """Delete a specific review for a specific menu item"""
+    return id
