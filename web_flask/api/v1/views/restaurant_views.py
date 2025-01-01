5923e042c816bfc66a68b2719fb188f26d616532
Tariq5mo <tariqomer756@gmail.com>
2025-01-01 08:17:23 +0200

    Add the restaurant view

---
+++ web_flask/api/v1/views/restaurant_views.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+"""
+from flask import jsonify
+from api import app_views
+from models import storage
+
+
+@app_views.route('/restaurants', strict_slashes=False)
+def get_restaurants():
+    """
+    """
+    return jsonify(storage.all("Restaurant"))
+
+
+@app_views.route('/restaurants/<id>', strict_slashes=False)
+def get_restaurant(id):
+    """Get specific restaurant"""
+    return id
