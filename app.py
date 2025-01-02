7f771d19d8adc4c5cd8dd39df89ebda21221f6bd
Tariq5mo <tariqomer756@gmail.com>
2025-01-02 13:08:56 +0200

    Update Flask app initialization to set template folder and API URL prefix

--- app.py
+++ app.py
@@ -4,9 +4,8 @@
 from flask import Flask
 from web_flask.api.v1.views import app_views
 
-
-foodify_app: Flask = Flask(__name__)
-foodify_app.register_blueprint(app_views)
+foodify_app: Flask = Flask(__name__, template_folder='web_flask/templates')
+foodify_app.register_blueprint(app_views, url_prefix='/api')
 
 
 if __name__ == "__main__":
