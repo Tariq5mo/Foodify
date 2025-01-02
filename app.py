f640ed03e376e359a1458ff34e1fcc22cc71d324
Tariq5mo <tariqomer756@gmail.com>
2025-01-02 13:10:00 +0200

    Add static folder to Flask app initialization

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from flask import Flask
 from web_flask.api.v1.views import app_views
 
-foodify_app: Flask = Flask(__name__, template_folder='web_flask/templates')
+foodify_app: Flask = Flask(__name__, template_folder='web_flask/templates', static_folder='web_flask/static')
 foodify_app.register_blueprint(app_views, url_prefix='/api')
 
 
